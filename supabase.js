4e2963373da98d2bc27e78a54ea00a0b4cb1e8e1
abhyvoid <abhayrangappanvat@gmail.com>
2025-08-24 20:49:07 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// supabase.js
+const SUPABASE_URL = "https://kfgjkkprfppnewhehzfr.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmZ2pra3ByZnBwbmV3aGVoemZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NTE0MTEsImV4cCI6MjA3MTUyNzQxMX0.OnfuZGz_c6SglErHOI6vGHt-Y44QHkThWXbZPw4ieSs";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
